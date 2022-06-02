f538d1a2cbdf862f349fa60af48b402edfff20d4
heesung33 <chaeheesung33@gmail.com>
2022-06-02 23:50:15 +0900

    🔫 수정

--- crud/src/contacts/contacts.controller.ts
+++ crud/src/contacts/contacts.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 import { Contact } from './contact.entity';
 import { ContactsService } from './contacts.service';
 import { Post, Put, Delete, Body, Param } from '@nestjs/common';
+import { DeleteResult, UpdateResult } from 'typeorm';
 
 @Controller('contacts')
 export class ContactsController {
@@ -13,19 +14,19 @@ export class ContactsController {
   }
 
   @Post('create')
-  async create(@Body() contactData: Contact): Promise<any> {
+  async create(@Body() contactData: Contact): Promise<Contact> {
     return this.contactsService.create(contactData);
   }
 
   @Put(':id/update')
-  async update(@Param('id') id, @Body() contactData: Contact): Promise<any> {
+  async update(@Param('id') id, @Body() contactData: Contact): Promise<UpdateResult> {
     contactData.id = Number(id);
     console.log('Update #' + contactData.id);
     return this.contactsService.update(contactData);
   }
 
   @Delete(':id/delete')
-  async delete(@Param('id') id): Promise<any> {
+  async delete(@Param('id') id): Promise<DeleteResult> {
     return this.contactsService.delete(id);
   }
 }
